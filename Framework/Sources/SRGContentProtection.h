c54192a331e70a3381a974477ecb1fd1730f41d1
Samuel Défago <defagos@gmail.com>
2018-09-18 09:11:11 +0200

    Make new header public

--- Framework/Sources/SRGContentProtection.h
+++ Framework/Sources/SRGContentProtection.h
@@ -11,3 +11,6 @@ FOUNDATION_EXPORT NSString *SRGContentProtectionMarketingVersion(void);
 
 //  Return `YES` iff run in a public (open source) setup. Always `YES`.
 FOUNDATION_EXPORT BOOL SRGContentProtectionIsPublic(void);
+
+// Public headers.
+#import "SRGContentProtectionConstants.h"
